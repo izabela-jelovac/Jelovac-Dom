ba255ddc93adef34931a0ec6c683123c30034cb8
Izabela <104584812+izabela-jelovac@users.noreply.github.com>
2023-01-05 00:27:15 +0100

    remove unnecessary files and styles, add introductory text

--- jelovac-dom/src/App.js
+++ jelovac-dom/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Jelovac Dom</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Potrebna Vam je ALU ili PVC stolarija? Želite li kuću za turizam?
+          Želite li napokon kupiti svoju kuću iz snova?{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Na pravom ste mjestu!
+          <n>Za više informacija obratite nam se s povjerenjem!</n>
+        </p>
       </header>
     </div>
   );
